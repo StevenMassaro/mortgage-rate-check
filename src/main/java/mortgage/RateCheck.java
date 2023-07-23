00923ade753bd4f1a4818283af6e363c429e84f5
Steven Massaro <steven.massaro.web@gmail.com>
2023-07-23 18:27:09 -0500

    attempt rate checks multiple times before failing

--- src/main/java/mortgage/RateCheck.java
+++ src/main/java/mortgage/RateCheck.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.time.Duration;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class RateCheck {
@@ -22,46 +23,64 @@ public class RateCheck {
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Starting rate check on " + new Date());
+        int attemptCount = 0;
+        int maxAttempts = 3;
+        while (attemptCount < maxAttempts) {
+            attemptCount++;
+            try {
+                tryRateCheck();
+            } catch (Exception e) {
+                System.out.println("Failed to do rate check on attempt " +attemptCount +"/"+maxAttempts);
+                e.printStackTrace();
+                TimeUnit.SECONDS.sleep(new Random().nextInt(60));
+            }
+        }
+    }
+
+    private static void tryRateCheck() throws InterruptedException {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless=new", "--no-sandbox");
         WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().setSize(new Dimension(1080, 1080));
+        try {
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            driver.manage().window().setSize(new Dimension(1080, 1080));
 
-        System.out.println("Loading rate page and autofilling...");
-        driver.get("https://member-mortgage.cuofco.org/#/rate-selection");
-        WebElement purchasePriceInputBox = findInputByName(driver, "purchasePrice");
-        purchasePriceInputBox.clear();
-        purchasePriceInputBox.sendKeys(PURCHASE_PRICE);
-        WebElement downPaymentDollars = driver.findElement(By.xpath("/html/body/platform-shell/div/public-layout/micro-app/div/pricing-index/div/consumer-pricing-container/div/mortgage-quote/div[1]/div[2]/div[3]/ui-range-slider/div[2]/ui-input/div/input"));
-        downPaymentDollars.clear();
-        downPaymentDollars.sendKeys(DOWN_PAYMENT);
-        WebElement zip = findInputByName(driver, "RateSelectionZipCode");
-        zip.sendKeys(ZIP_CODE);
-        WebElement creditScoreDropdown = driver.findElement(By.xpath("/html/body/platform-shell/div/public-layout/micro-app/div/pricing-index/div/consumer-pricing-container/div/mortgage-quote/div[1]/div[2]/div[5]/div[2]/ui-container/div/ui-options/div[1]/div/input"));
-        creditScoreDropdown.click();
-        WebElement creditScore780 = driver.findElement(By.xpath("/html/body/platform-shell/div/public-layout/micro-app/div/pricing-index/div/consumer-pricing-container/div/mortgage-quote/div[1]/div[2]/div[5]/div[2]/ui-container/div/ui-options/div[1]/div/ul/li[2]"));
-        creditScore780.click();
-        WebElement propertyTypeDropdown = driver.findElement(By.xpath("/html/body/platform-shell/div/public-layout/micro-app/div/pricing-index/div/consumer-pricing-container/div/mortgage-quote/div[1]/div[2]/div[6]/div[2]/ui-container/div/ui-options/div[1]/div/input"));
-        propertyTypeDropdown.click();
-        WebElement singleFamilyHome = driver.findElement(By.xpath("/html/body/platform-shell/div/public-layout/micro-app/div/pricing-index/div/consumer-pricing-container/div/mortgage-quote/div[1]/div[2]/div[6]/div[2]/ui-container/div/ui-options/div[1]/div/ul/li[2]"));
-        singleFamilyHome.click();
-        TimeUnit.SECONDS.sleep(30);
-        System.out.println("Loading rates...");
-        WebElement seeMyRatesButton = driver.findElement(By.id("seeMyRatesButton"));
-        seeMyRatesButton.click();
-        TimeUnit.SECONDS.sleep(60);
-        System.out.println("Taking screenshot...");
-        byte[] results = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-        driver.quit();
+            System.out.println("Loading rate page and autofilling...");
+            driver.get("https://member-mortgage.cuofco.org/#/rate-selection");
+            WebElement purchasePriceInputBox = findInputByName(driver, "purchasePrice");
+            purchasePriceInputBox.clear();
+            purchasePriceInputBox.sendKeys(PURCHASE_PRICE);
+            WebElement downPaymentDollars = driver.findElement(By.xpath("/html/body/platform-shell/div/public-layout/micro-app/div/pricing-index/div/consumer-pricing-container/div/mortgage-quote/div[1]/div[2]/div[3]/ui-range-slider/div[2]/ui-input/div/input"));
+            downPaymentDollars.clear();
+            downPaymentDollars.sendKeys(DOWN_PAYMENT);
+            WebElement zip = findInputByName(driver, "RateSelectionZipCode");
+            zip.sendKeys(ZIP_CODE);
+            WebElement creditScoreDropdown = driver.findElement(By.xpath("/html/body/platform-shell/div/public-layout/micro-app/div/pricing-index/div/consumer-pricing-container/div/mortgage-quote/div[1]/div[2]/div[5]/div[2]/ui-container/div/ui-options/div[1]/div/input"));
+            creditScoreDropdown.click();
+            WebElement creditScore780 = driver.findElement(By.xpath("/html/body/platform-shell/div/public-layout/micro-app/div/pricing-index/div/consumer-pricing-container/div/mortgage-quote/div[1]/div[2]/div[5]/div[2]/ui-container/div/ui-options/div[1]/div/ul/li[2]"));
+            creditScore780.click();
+            WebElement propertyTypeDropdown = driver.findElement(By.xpath("/html/body/platform-shell/div/public-layout/micro-app/div/pricing-index/div/consumer-pricing-container/div/mortgage-quote/div[1]/div[2]/div[6]/div[2]/ui-container/div/ui-options/div[1]/div/input"));
+            propertyTypeDropdown.click();
+            WebElement singleFamilyHome = driver.findElement(By.xpath("/html/body/platform-shell/div/public-layout/micro-app/div/pricing-index/div/consumer-pricing-container/div/mortgage-quote/div[1]/div[2]/div[6]/div[2]/ui-container/div/ui-options/div[1]/div/ul/li[2]"));
+            singleFamilyHome.click();
+            TimeUnit.SECONDS.sleep(30);
+            System.out.println("Loading rates...");
+            WebElement seeMyRatesButton = driver.findElement(By.id("seeMyRatesButton"));
+            seeMyRatesButton.click();
+            TimeUnit.SECONDS.sleep(60);
+            System.out.println("Taking screenshot...");
+            byte[] results = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
 
-        System.out.println("Sending Telegram message...");
-        // send telegram message with details
-        TelegramBot telegramBot = new TelegramBot(BOT_TOKEN);
-        SendPhoto sendPhoto = new SendPhoto(CHAT_ID, results);
-        SendResponse sendResponse = telegramBot.execute(sendPhoto);
-        boolean ok = sendResponse.isOk();
-        System.out.println("Completed rate check on " + new Date());
+            System.out.println("Sending Telegram message...");
+            // send telegram message with details
+            TelegramBot telegramBot = new TelegramBot(BOT_TOKEN);
+            SendPhoto sendPhoto = new SendPhoto(CHAT_ID, results);
+            SendResponse sendResponse = telegramBot.execute(sendPhoto);
+            boolean ok = sendResponse.isOk();
+            System.out.println("Completed rate check on " + new Date());
+        } finally {
+            driver.quit();
+        }
     }
 
     private static WebElement findInputByName(WebDriver driver, String name) {
